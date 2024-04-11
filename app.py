c2f6bdc62c0379427e57a721cda2b5b70f9ac4e9
Inna <inna8923370@yandex.ru>
2024-04-11 21:14:49 +0700

    Initial Commit

--- app.py
+++ app.py
@@ -3,9 +3,12 @@ from data import db_session
 from flask import render_template, request, redirect
 from data.users import Article
 from flask_login import LoginManager, login_user
-from data.login import RegisterForm
+#from data.login import RegisterForm
 import numpy as np
 import pyaudio as pa
+from data.registr import User
+from data.login import LoginForm
+
 
 # частота дискретизации
 sample_rate = 44100
@@ -23,6 +26,11 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 login_manager = LoginManager()
 login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)
 
 def generate_sample(freq, duration, volume):
     # амплитуда
@@ -85,7 +93,7 @@ def load_user(user_id):
     return db_sess.query(Article).get(user_id)
 
 
-@app.route('/login', methods=['GET', 'POST'])
+'''@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = RegisterForm()
     if form.validate_on_submit():
@@ -93,9 +101,22 @@ def login():
             return render_template('register.html', title='Регистрация',
                                    form=form,
                                    message="Пароли не совпадают")
-    return render_template('login.html', title='Авторизация', form=form)
+    return render_template('login.html', title='Авторизация', form=form)'''
 
-
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if form.password.data == 123:
+            if user and user.check_password(form.password.data):
+                login_user(user, remember=form.remember_me.data)
+                return redirect("/")
+            return render_template('login.html',
+                                   message="Неправильный логин или пароль",
+                                   form=form)
+        return render_template('login.html', title='Авторизация', form=form)
 @app.route('/instruments')
 def index():
     return render_template('index.html')
--- data/registr.py
+++ data/registr.py
@@ -1,6 +1,7 @@
-
-
-class UserLogin():
+from flask_login import UserMixin
+from .db_session import SqlAlchemyBase
+class User(SqlAlchemyBase, UserMixin):
+    __tablename__ = 'users'
     def is_authenticated(self):
         return True
 
